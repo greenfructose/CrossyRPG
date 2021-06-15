2206b4f03fd03749883ce71800dc0e32a37baab6
greenfructose <justinclarkturney@gmail.com>
2021-06-14 19:47:55 -0500

    Fixed bug where player can get stuck to boundaries

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(GameObject):
         self.speed = speed
 
     def move(self, direction, max_height):
-        if self.y >= (max_height - self.height) or self.y == 0:
+        if (self.y >= max_height - self.height and direction > 0) or (self.y == 0 and direction < 0):
             return
         else:
             self.y += (direction * self.speed)
